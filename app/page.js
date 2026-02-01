90e80ea965de4bc7f27264e2d388395fa22401fe
savvypanwar <uttamturkar3737@gmail.com>
2026-02-01 23:04:08 +0530

    new update

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Header from "@/component/Header/Header";
 import RiderDashboard from "./rider-dashboard/page";
+import Navbar from "../component/Navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Header />
+      <Navbar />
       <RiderDashboard />
     </div>
   );
