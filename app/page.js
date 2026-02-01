45321f4c01c1309313553924388f5bd70939388f
mohit13158921 <mohitrahangdale89@gmail.com>
2026-02-01 23:54:32 +0530

    footer

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "@/component/Header/Header";
 
+
 export default function Home() {
   return (
     <div>
-      <Header />
+    
+      
     </div>
   );
 }
